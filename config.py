191a92502cb95bb1c98b20767916a79e727583ee
Daniel Svens <danielsvens@hotmail.com>
2022-10-01 13:39:55 +0200

    updated cfg

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DEBUG = False
 TESTING = False
-SECRET_KEY = os.environ["SECRET"]
-SQLALCHEMY_DATABASE_URI = f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}'
+SECRET_KEY = os.environ.get('SECRET')
+DB_URI = f'postgresql://{os.environ["DB_USERNAME"]}:{os.environ["DB_PASSWORD"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB_NAME"]}'
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 USE_RELOADER = False
 ENV = 'production'
